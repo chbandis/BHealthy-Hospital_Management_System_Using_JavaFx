6187ded157eb613617fdd63f61ed2f6ffd7a6196
Christos Bandis <chr.bandis@gmail.com>
2022-11-26 20:44:07 +0200

    Update AddDiseaseController.java

--- src/Controllers/AddDiseaseController.java
+++ src/Controllers/AddDiseaseController.java
@@ -30,26 +30,26 @@ public class AddDiseaseController implements Initializable {
     @FXML private TextField disCat;
     int id;
 
-    //Μέθοδος για το κουμπί Cancel
+    //Method for the Cancel button
     public void handleCancelDisBtn(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("diseases.fxml")); //Καθορισμός του diseases.fxml ως Parent
-        Scene DisScene = new Scene(root); //Δημιουργία του Scene "DisScene"
-        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow(); //Δημιουργία του Stage "window"
-        window.setScene(DisScene); //Καθοριμός του Scene "DisScene" στο Stage "window"
-        window.show(); //Εμφάνιση του Stage "window"
+        Parent root = FXMLLoader.load(getClass().getResource("diseases.fxml")); //Specify diseases.fxml as Parent
+        Scene DisScene = new Scene(root); //Creating the Scene "DisScene"
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow(); //Create the Stage "window"
+        window.setScene(DisScene); //Set the "DisScene" Scene in the "window" Stage
+        window.show(); //Show the Stage "window"
     }
-    //Μέθοδος για το κουμπί Add Disease
+    //Method for Add Disease button
     public void addDis(ActionEvent event) throws IOException {
-        String name = disName.getText(); //Καταχώριση των τιμών των πεδίων στις αντίστοιχες μεταβλητές
+        String name = disName.getText(); //Enter the field values in the corresponding variables
         String desc = disDesc.getText();
         String cat = disCat.getText();
-        if(name.isEmpty() || desc.isEmpty() || cat.isEmpty()){ //Έλεγχος για κενά πεδία και εμφάνιση του κατάλληλου μηνύματος
+        if(name.isEmpty() || desc.isEmpty() || cat.isEmpty()){ //Check for empty fields and display the appropriate message
             MessageBox.SimpleMBox("Please fill all the fields", "Fill all the fields");
-        } else if(containsNumber(name) || containsNumber(desc) || containsNumber(cat)){ //Έλεγχος αν οι τιμές των πεδίων περιέχουν αριθμούς μέσω της μεθόδου "containsNumber"
-            MessageBox.SimpleMBox("Textfields should not contain number(s)", "Error");      //που δημιουργείται παρακάτω και εμφάνιση του κατάλληλου μηνύματος
+        } else if(containsNumber(name) || containsNumber(desc) || containsNumber(cat)){ //Check if field values contain numbers by using the containsnumber method
+            MessageBox.SimpleMBox("Textfields should not contain number(s)", "Error");      //created below and display the appropriate message
         }
-        else { //Αν ολοκληρωθούν οι έλεγχοι χωρίς κάποιο σφάλμα πραγματοποιείται εγγραφή των τιμών στη "βάση" μέσω της μεθόδου "addDisease" της κλάσης "writeToDB"
-            writeToDB.addDisease(id, name, desc, cat);                                                   //και ανακατεύθυνση Stage "window" που δημιουργείται παρακάτω
+        else { //if the tests are completed without any error, the values are written to the "database" through the "addDisease" method of the "writeToDB" class
+            writeToDB.addDisease(id, name, desc, cat);                                                   //and redirect to stage "window" created below
             Parent root = FXMLLoader.load(getClass().getResource("diseases.fxml"));
             Scene DisScene = new Scene(root);
             Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
@@ -57,7 +57,7 @@ public class AddDiseaseController implements Initializable {
             window.show();
         }
     }
-    //Η μέθοδος containsNumber που δέχεται μία μεταβλητή τύπου "String" και επιστρέφει αν αυτή η μεταβλητή περιέχει αριθμούς.
+    //the containsnumber method that accepts a string variable and returns if that variable contains numbers
     public boolean containsNumber(String s)
     {
         Pattern p = Pattern.compile( "[0-9]" );
@@ -66,26 +66,26 @@ public class AddDiseaseController implements Initializable {
         return m.find();
     }
 
-    //Χρήση της μεθόδου initialize ώστε να αρχικοποιηθούν οι παρακάτω τιμές πριν την εκτέλεση του Controller
+    //use the initialize method to initialize the following values before running the controller
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         int lastid = 0;
         String line = "";
         String delimiter = ":";
         String Disfile = "database/diseaseDB.txt";
-        try (BufferedReader br = new BufferedReader(new FileReader(Disfile))) { //Reading του αρχείου diseaseDB.txt
+        try (BufferedReader br = new BufferedReader(new FileReader(Disfile))) { //Reading the file diseaseDB.txt
             while ((line = br.readLine()) != null) {
                 List<String> values = Arrays.asList(line.split(delimiter));
-                lastid = Integer.parseInt(values.get(0)); //Καταχώριση της πρώτης τιμής του αρχείου στη μεταβλητή lastid
+                lastid = Integer.parseInt(values.get(0)); //Enter the first value of the file in the lastid variable
             }
-            if(lastid==0) { //Αν δεν υπάρχει τιμή στο αρχείο τότε καταχωρείται στη μεταβλητή id η τιμή 1
-                id = 1;     //ώστε οι εγγραφές να ξεκινήσουν από τον αριθμό 1 και όχι απο το 0
+            if(lastid==0) { //if there is no value in the file then the value 1 is entered in the variable id
+                id = 1;     //so that registrations start from the number 1 and not from 0
             } else {
-                id = lastid + 1; //Αν υπάρχει τιμή στο αρχείο τότε η πρώτη τιμή της επόμενης εγγραφής που θα πραγματοποιηθεί θα
-            }                    //θα είναι η τιμή της προηγούμενης αυξημένη κατά 1
+                id = lastid + 1; //if there is a value in the file then the first value of the next record that will be made 
+            }                    //will be the value of the previous increased by 1
         } catch (IOException e) {
-            e.printStackTrace(); //Σε αυτό το σημείο δε πραγματοποιείται έλεγχος για την ύπαρξη του αρχείου "diseaseDB", καθώς πραγματοποιείται
-        }                        //στο προηγούμενο Stage και στη περίπτωση που δεν υφίσταται το συγκεκριμένο αρχείο δεν εμφανίζεται το παρόν Stage (addDisease.fxml)
+            e.printStackTrace(); //At this point there is no check for the existence of the "diseaseDB" file, as it is performed
+        }                        //in the previous stage and if the specific file does not exist the present stage does not appear (adddisease.fxml)
     }
 }
 
