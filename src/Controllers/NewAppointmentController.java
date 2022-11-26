f9ffeaf2a733f45eada560e1e134f8cbae60b2de
Christos Bandis <chr.bandis@gmail.com>
2022-11-26 21:28:49 +0200

    Update NewAppointmentController.java

--- src/Controllers/NewAppointmentController.java
+++ src/Controllers/NewAppointmentController.java
@@ -44,17 +44,17 @@ public class NewAppointmentController implements Initializable {
     ObservableList<String> allDis = FXCollections.observableArrayList();
     int id;
 
-    //Μέθοδος για το κουμπί Cancel
+    //Method for the Cancel button
     public void handleCancelAppointBtn(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("appointments.fxml")); //Καθορισμός του appointments.fxml ως Parent
-        Scene AppointScene = new Scene(root); //Δημιουργία του Scene "AppointScene"
-        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow(); //Δημιουργία του Stage "window"
-        window.setScene(AppointScene); //Καθοριμός του Scene "AppointScene" στο Stage "window"
-        window.show(); //Εμφάνιση του Stage "window"
+        Parent root = FXMLLoader.load(getClass().getResource("appointments.fxml")); //Set appointments.fxml as Parent
+        Scene AppointScene = new Scene(root); //Create Scene "AppointScene"
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow(); //Create the Stage "window"
+        window.setScene(AppointScene); //Set of Scene "AppointScene" in stage "window"
+        window.show(); //Show the Stage "window"
     }
-    //Μέθοδος για το κουμπί New Appointment
+    //Method for the New Appointment button
     public void newAppoint(ActionEvent event) throws IOException {
-        String pname = (String)  patName.getValue(); //Καταχώριση των τιμών των πεδίων στις αντίστοιχες μεταβλητές
+        String pname = (String)  patName.getValue(); //Enter the field values in the corresponding variables
         String tel = patTel.getText();
         String dname = (String) docName.getValue();
         String date = String.valueOf(appointDate.getValue());
@@ -62,13 +62,13 @@ public class NewAppointmentController implements Initializable {
         String patdis = (String) dis.getValue();
         String pattreat = treat.getText();
 
-        //Έλεγχος για κενά πεδία και εμφάνιση του κατάλληλου μηνύματος
+        //Check for empty fields and display the appropriate message
         if(patName.getSelectionModel().isEmpty() || tel.isEmpty() || docName.getSelectionModel().isEmpty() || appointDate.getValue() == null  || Hpicker.getSelectionModel().isEmpty() || Mpicker.getSelectionModel().isEmpty() || dis.getSelectionModel().isEmpty() || pattreat.isEmpty()){
             MessageBox.SimpleMBox("Please fill all the fields", "Fill all the fields");
         } else if (!validateMobileNo(patTel)) {
             MessageBox.SimpleMBox("Please enter a valid phone number", "Error");
-        } else { //Αν ολοκληρωθεί ο παραπάνω έλεγχος χωρίς κάποιο σφάλμα πραγματοποιείται εγγραφή των τιμών στη "βάση" μέσω της μεθόδου "newAppointment" της κλάσης "writeToDB"
-            writeToDB.newAppointment(id, pname, tel, dname, date, datetime, patdis, pattreat);                        //και ανακατεύθυνση Stage "window" που δημιουργείται παρακάτω
+        } else { //If the above check is completed without any error, the values are written to the "database" through the "newAppointment" method of the "writeToDB" class
+            writeToDB.newAppointment(id, pname, tel, dname, date, datetime, patdis, pattreat);                        //and redirect to stage "window" created below
             Parent root = FXMLLoader.load(getClass().getResource("appointments.fxml"));
             Scene DisScene = new Scene(root);
             Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
@@ -76,14 +76,14 @@ public class NewAppointmentController implements Initializable {
             window.show();
         }
     }
-    //Η μέθοδος validateMobileNo που δέχεται ένα Textfield και επιστρέφει αν το περιεχόμενο του αντιστοιχεί σε πραγματικό αριθμό τηλεφώνου δέκα ψηφίων.
+    //The validateMobileNo method that accepts a Textfield and returns if its content corresponds to a real ten-digit phone number.
     private boolean validateMobileNo(TextField tel) {
         Pattern p = Pattern.compile("^\\d{10}$");
         Matcher m = p.matcher(tel.getText());
         return m.find();
     }
 
-    //Η μέθοδος docSpecChoice που θέτει τις τιμές της κατάλληλης λίστας, ανάλογα με το όνομα του γιατρού, στα items του Combobox "dis"
+    //The docSpecChoice method that sets the values of the appropriate list, depending on the name of the doctor, in the Combobox items "dis"
    @FXML
     private void docSpecChoice(){
         if(docName.getValue().equals("Dr Cheek")){
@@ -99,10 +99,10 @@ public class NewAppointmentController implements Initializable {
         }
     }
 
-    //Χρήση της μεθόδου initialize ώστε να αρχικοποιηθούν οι παρακάτω τιμές πριν την εκτέλεση του Controller
+    //use the initialize method to initialize the following values before running the controller
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        Hpicker.setValue("Hour"); //Εισαγωγή των κατάλληλων τιμών στα ComboBoxes "Hpicker" και "Mpicker"
+        Hpicker.setValue("Hour"); //Entering the appropriate values in the ComboBoxes "Hpicker" and "Mpicker"
         Mpicker.setValue("Minutes");
         for(int l=8; l<10; l++){
             Hpicker.getItems().add("0" + l);
@@ -120,13 +120,13 @@ public class NewAppointmentController implements Initializable {
         String line = "";
         String delimiter = ":";
         String Docfile = "database/doctorDB.txt";
-        try (BufferedReader br = new BufferedReader(new FileReader(Docfile))) { //Reading του αρχείου doctorDB.txt
+        try (BufferedReader br = new BufferedReader(new FileReader(Docfile))) { //Reading the file doctorDB.txt
             while ((line = br.readLine()) != null) {
                 List<String> values = Arrays.asList(line.split(delimiter));
-                docs.add(values.get(1)); //Καταχώριση της δεύτερης τιμής της κάθε εγγραφής που περιέχει το αρχείο στη λίστα "docs"
+                docs.add(values.get(1)); //Enter the second value of each record that contains the file in the "docs" list
             }
-            docName.setItems(docs); //Εισαγωγή των τιμών της λίστας docs στα items του ComboBox "docName"
-        } catch (FileNotFoundException e){ //Αν δεν υπάρχει το αρχείο "doctorDB" τότε πραγματοποιούνται οι κατάλληλες ενέργειες
+            docName.setItems(docs); //Entering the values of the docs list in the ComboBox items "docName"
+        } catch (FileNotFoundException e){ //if the "doctordb" file does not exist then appropriate actions are taken
             docName.setValue("Error connecting to Doctor Database");
             btnSave.setDisable(true);
         } catch (IOException e) {
@@ -134,12 +134,12 @@ public class NewAppointmentController implements Initializable {
         }
 
         String dermDisfile = "database/diseaseDB.txt";
-        try (BufferedReader br = new BufferedReader(new FileReader(dermDisfile))) { //Reading του αρχείου diseaseDB.txt
+        try (BufferedReader br = new BufferedReader(new FileReader(dermDisfile))) { //Reading the file diseaseDB.txt
             while ((line = br.readLine()) != null) {
                 List<String> values = Arrays.asList(line.split(delimiter));
                 allDis.add(values.get(1));
-                if (values.get(3).equals("Dermatology")){ //Καταχώριση της δεύτερης τιμής της κάθε εγγραφής που περιέχει το αρχείο στη κατάλληλη λίστα, ανάλογα
-                    dermDis.add(values.get(1));            //με το περιεχόμενο της τέταρτης τιμής κάθε εγγραφής του αρχείου
+                if (values.get(3).equals("Dermatology")){ //Enter the second value of each record that contains the file in the appropriate list,
+                    dermDis.add(values.get(1));            //depending on the content of the fourth value of each record in the file
                 }
                 else if (values.get(3).equals("Gastroenterology")){
                     gastroDis.add(values.get(1));
@@ -148,8 +148,8 @@ public class NewAppointmentController implements Initializable {
                     neuroDis.add(values.get(1));
                 }
             }
-            dis.getItems().add("Please select a doctor"); //Εισαγωγή τιμής στα items του ComboBox "dis"
-        } catch (FileNotFoundException e){ //Αν δεν υπάρχει το αρχείο "diseaseDB" τότε πραγματοποιούνται οι κατάλληλες ενέργειες
+            dis.getItems().add("Please select a doctor"); //Enter a value in the ComboBox items "dis"
+        } catch (FileNotFoundException e){ //if the "diseasedb" file does not exist then appropriate actions are taken
             dis.setValue("Error connecting to Disease Database");
             btnSave.setDisable(true);
         } catch(IOException e){
@@ -157,12 +157,12 @@ public class NewAppointmentController implements Initializable {
         }
 
         String Patfile = "database/patientDB.txt";
-        try (BufferedReader br = new BufferedReader(new FileReader(Patfile))) { //Reading του αρχείου patientDB.txt
+        try (BufferedReader br = new BufferedReader(new FileReader(Patfile))) { //Reading the file patientDB.txt
             while ((line = br.readLine()) != null) {
                 List<String> values = Arrays.asList(line.split(delimiter));
-                patName.getItems().add(values.get(1)); //Καταχώριση της δεύτερης τιμής της κάθε εγγραφής που περιέχει το αρχείο στo ComboBox "patName"
+                patName.getItems().add(values.get(1)); //Enter the second value of each record that contains the file in the "patName" ComboBox
             }
-        } catch (FileNotFoundException e){ //Αν δεν υπάρχει το αρχείο "patientDB" τότε πραγματοποιούνται οι κατάλληλες ενέργειες
+        } catch (FileNotFoundException e){ //if the "patientdb" file does not exist then appropriate actions are taken
             patName.setValue("Error connecting to Patient Database");
             btnSave.setDisable(true);
         } catch (IOException e) {
@@ -171,22 +171,18 @@ public class NewAppointmentController implements Initializable {
 
         int lastid = 0;
         String Appointfile = "database/appointmentDB.txt";
-        try (BufferedReader br = new BufferedReader(new FileReader(Appointfile))) { //Reading του αρχείου appointmentDB.txt
+        try (BufferedReader br = new BufferedReader(new FileReader(Appointfile))) { //Reading the file appointmentDB.txt
             while ((line = br.readLine()) != null) {
                 List<String> values = Arrays.asList(line.split(delimiter));
-                lastid = Integer.parseInt(values.get(0)); //Καταχώριση της πρώτης τιμής του αρχείου στη μεταβλητή lastid
+                lastid = Integer.parseInt(values.get(0)); //Enter the first value of the file in the lastid variable
             }
-            if(lastid==0) {  //Αν δεν υπάρχει τιμή στο αρχείο τότε καταχωρείται στη μεταβλητή id η τιμή 1
-                id = 1;      //ώστε οι εγγραφές να ξεκινήσουν από τον αριθμό 1 και όχι απο το 0
+            if(lastid==0) {  //if there is no value in the file then the value 1 is entered in the variable id
+                id = 1;      //so that registrations start from the number 1 and not from 0
             } else {
-                id = lastid + 1;  //Αν υπάρχει τιμή στο αρχείο τότε η πρώτη τιμή της επόμενης εγγραφής που θα πραγματοποιηθεί θα
-            }                     //θα είναι η τιμή της προηγούμενης αυξημένη κατά 1
+                id = lastid + 1;  //if there is a value in the file then the first value of the next record that will be made 
+            }                     //will be the value of the previous increased by 1
         } catch (IOException e) {
-            e.printStackTrace();  //Σε αυτό το σημείο δε πραγματοποιείται έλεγχος για την ύπαρξη του αρχείου "appointmentDB", καθώς πραγματοποιείται
-        }                         //στο προηγούμενο Stage και στη περίπτωση που δεν υφίσταται το συγκεκριμένο αρχείο δεν εμφανίζεται το παρόν Stage (newAppointment.fxml)
+            e.printStackTrace();  //At this point there is no check for the existence of the "appointmentDB" file, as it is carried out
+        }                         //in the previous Stage and in case the specific file does not exist, the present Stage does not appear (newAppointment.fxml)
     }
 }
-
-
-
-
