4de6b07fd18335e4817081d179f42ad7e2a8bd52
Christos Bandis <chr.bandis@gmail.com>
2022-11-26 21:20:36 +0200

    Update DoctorController.java

--- src/Controllers/DoctorController.java
+++ src/Controllers/DoctorController.java
@@ -32,15 +32,15 @@ public class DoctorController implements Initializable {
     @FXML private Button btnAddDoc;
     @FXML private Button btnCreateDatabase;
 
-    //Μέθοδος για το κουμπί Manage Appointments
+    //Method for the Manage Appointments button
     public void handleAppointBtn(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("appointments.fxml"));  //Καθορισμός του appointments.fxml ως Parent
-        Scene AppointScene = new Scene(root); //Δημιουργία του Scene "AppointScene"
-        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();  //Δημιουργία του Stage "window"
-        window.setScene(AppointScene); //Καθοριμός του Scene "AppointScene" στο Stage "window"
-        window.show(); //Εμφάνιση του Stage "window"
+        Parent root = FXMLLoader.load(getClass().getResource("appointments.fxml"));  //Set the appointments.fxml as Parent
+        Scene AppointScene = new Scene(root); //Create Scene "AppointScene"
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();  //Create the Stage "window"
+        window.setScene(AppointScene); //Set of Scene "AppointScene" in stage "window"
+        window.show(); //Show the Stage "window"
     }
-    //Μέθοδος για το κουμπί Manage Patients
+    //Method for the Manage Patients button
     public void handlePatBtn(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("patients.fxml"));
         Scene PatScene = new Scene(root);
@@ -48,7 +48,7 @@ public class DoctorController implements Initializable {
         window.setScene(PatScene);
         window.show();
     }
-    //Μέθοδος για το κουμπί Manage Diseases
+    //Method for the Manage Diseases button
     public void handleDisBtn(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("diseases.fxml"));
         Scene DisScene = new Scene(root);
@@ -56,7 +56,7 @@ public class DoctorController implements Initializable {
         window.setScene(DisScene);
         window.show();
     }
-    //Μέθοδος για το κουμπί Add Doctor
+    //Method for the Add Doctor button
     public void handleAddDocBtn(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("addDoctor.fxml"));
         Scene DisScene = new Scene(root);
@@ -64,7 +64,7 @@ public class DoctorController implements Initializable {
         window.setScene(DisScene);
         window.show();
     }
-    //Μέθοδος για το κουμπί Create Database
+    //Method for the Create Database button
     public void handleCreateDBbtn(ActionEvent event) {
         try {
             FileWriter myWriter = new FileWriter("database/doctorDB.txt", true);
@@ -82,29 +82,29 @@ public class DoctorController implements Initializable {
         }
     }
 
-    //Χρήση της μεθόδου initialize ώστε να αρχικοποιηθούν οι παρακάτω τιμές πριν την εκτέλεση του Controller
+    //use the initialize method to initialize the following values before running the controller
     @Override
     public void initialize(URL url, ResourceBundle rb){
-        //Εισαγωγή των τιμών των Object της κλάσης "Doctor" στα columns του Table "doctorTable"
+        //Enter the Object values of the "Doctor" class in the Table "doctorTable" columns
         doctorIDColumn.setCellValueFactory(new PropertyValueFactory<Doctor, Integer>("id"));
         doctorNameColumn.setCellValueFactory(new PropertyValueFactory<Doctor, String>("name"));
         doctorSpecialisationColumn.setCellValueFactory(new PropertyValueFactory<Doctor, String>("specialisation"));
 
-        DoctorTable.setItems(getDoctors()); //Εισαγωγή των τιμών που επιστρέφει η μέθοδος "getDoctors()" στο πίνακα "doctorTable"
+        DoctorTable.setItems(getDoctors()); //Enter the values returned by the "getDoctors()" method in the doctorTable table
     }
     public ObservableList<Doctor> getDoctors() {
         ObservableList<Doctor> doctors = FXCollections.observableArrayList();
         String file = "database/doctorDB.txt";
         String line = "";
         String delimiter = ":";
-        try (BufferedReader br = new BufferedReader(new FileReader(file))) { //Reading του αρχείου doctorDB.txt
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) { //Reading the file doctorDB.txt
             while ((line = br.readLine()) != null) {
                 List<String> values = Arrays.asList(line.split(delimiter));
                 int id = Integer.parseInt(values.get(0));
-                //Όσο το αρχείο περιέχει εγγραφές, δημιουργούνται objects με τα περιεχόμενα των εγγραφών αυτών και εισάγωνται στη λίστα "doctors"
+                //as long as the file contains records, objects with the contents of those records are created and entered into the doctors list
                 doctors.add(new Doctor(id, values.get(1), values.get(2)));
             }
-        } catch (FileNotFoundException e){ //Αν δεν υπάρχει το αρχείο "doctorDB" τότε πραγματοποιούνται οι κατάλληλες ενέργειες
+        } catch (FileNotFoundException e){ //if the "doctordb" file does not exist then appropriate actions are taken
            MessageBox.SimpleMBox("Could not connect to Database", "Error");
            btnAddDoc.setVisible(false);
            btnCreateDatabase.setVisible(true);
